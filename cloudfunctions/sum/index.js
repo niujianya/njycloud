e6ac4610fb0572ce9fa59ab14a8f1a53c41eb802
niujianya <645148258@qq.com>
2020-04-15 20:16:11 +0800

    first commit

---
+++ cloudfunctions/sum/index.js
@@ -0,0 +1,16 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = (event, context) => {
+  console.log(event)
+  console.log(context)
+  
+  const wxContext = cloud.getWXContext()
+  console.log(wxContext.OPENID)
+  return {
+    sum: event.a + event.b
+  }
+}
